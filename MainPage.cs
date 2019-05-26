a975b9639106e78d0049b3a04766b53e23bc93dc
Preslav Balabanov <preslav.balabanov@outlook.com>
2019-05-26 22:04:34 +0200

    First commit

---
+++ MainPage.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace OOP
+{
+    class MainPage
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is a compilation of all the exercises I have completed while doing my C# Developer course. \n\n" +
+                "DISCLAIMER:\n" +
+                "Since I had some prior knowledge of programming I skipped some chapters which are the same for all\n" +
+                "programming languages (i.e: data types, loops, if-else statements, switch cases, etc.)\n\n" +
+                "Enjoy!");
+
+            Console.WriteLine("Choose an exercise: \n" +
+                "[0] Overloading Methods Exercise\n" +
+                "[1] \n" +
+                "More coming soon!");
+            string input = Console.ReadLine();
+
+            switch (input)
+            {
+                default: Console.WriteLine("No Module was selected or wrong input was given");  break;
+                case "0": OverLoadingExercise(); break;
+            }
+
+        }
+
+        private static void OverLoadingExercise()
+        {
+            OverloadingExercise.OverloadingExerciseMain();
+        }
+
+
+    }
+}
---
+++ OverloadingExercise.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP
+{
+    class OverloadingExercise
+    {
+        public static void OverloadingExerciseMain()
+        {
+            int a = 5, b = 77, c = 789;
+            double x = 5.678, y = 77.345, z = 789.4643;
+
+            Console.WriteLine("Choose a calculation: \n" +
+                "[1] a + b = ? \n" +
+                "[2] a + b + c = ? \n" +
+                "[3] x + y = ? \n" +
+                "[4] x + y + z = ? \n");
+
+            string choice = Console.ReadLine();
+            switch (choice)
+            {
+                case "1": Calculate(a, b); break;
+                case "2": Calculate(a, b, c); break;
+                case "3": Calculate(x, y); break;
+                case "4": Calculate(x, y, z); break;
+                default: Calculate(); break;
+            }
+
+
+        }
+
+        static void Calculate()
+        {
+            Console.WriteLine("No values or wrong values were entered");
+        }
+
+        static void Calculate(int a, int b)
+        {
+            Console.WriteLine("Result = " + a + b );
+
+        }
+
+        static void Calculate(int a, int b, int c)
+        {
+            Console.WriteLine("Result = " + a + b + c);
+        }
+
+        static void Calculate(double x, double y)
+        {
+            Console.WriteLine("Result = " + x + y);
+        }
+
+        static void Calculate(double x, double y, double z)
+        {
+            Console.WriteLine("Result = " + x + y + z);
+        }
+    }
+}
